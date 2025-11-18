b114ddfc045b2fc786bb5d89133fe9f55d3b3e3b
manhtuong0619 <mtuong669@gmail.com>
2025-11-18 16:08:29 +0700

    fix: logo dpay in pwa

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -4,6 +4,29 @@ export default defineConfig({
   headLinkOptions: {
     preset: '2023',
   },
-  preset,
-  images: ['public/favicon.ico'],
+  preset: {
+    ...preset,
+    transparent: {
+      ...preset.transparent,
+      sizes: [64, 192, 512],
+      favicons: [[48, 'favicon.ico']],
+    },
+    maskable: {
+      ...preset.maskable,
+      sizes: [512],
+      padding: 0,
+      resizeOptions: {
+        background: '#1712FF',
+      },
+    },
+    apple: {
+      ...preset.apple,
+      sizes: [180],
+      padding: 0,
+      resizeOptions: {
+        background: '#1712FF',
+      },
+    },
+  },
+  images: ['public/pwa-512.png'],
 });
